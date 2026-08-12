df009471c675a9cc739eb33e6770880c271b2059
Brandonpyjv <brandonar1997@gmail.com>
2026-08-12 10:37:18 -0500

    Create UsuarioResponse.java

---
+++ src/main/java/com/sena/examenes/infrastructure/adapter/in/rest/UsuarioResponse.java
@@ -0,0 +1,10 @@
+package com.sena.examenes.infrastructure.adapter.in.rest;
+
+import com.sena.examenes.domain.model.Usuario;
+
+public record UsuarioResponse(String username, String email, boolean activo) {
+    public static UsuarioResponse desde(Usuario usuario) {
+        return new UsuarioResponse(usuario.getUsername(), usuario.getEmail(),
+                usuario.isActivo());
+    }
+}

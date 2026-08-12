033b10b61a9f50e6f16c304b21707c8025aacf0f
Brandonpyjv <brandonar1997@gmail.com>
2026-08-12 10:01:57 -0500

    Create UsuarioRepositoryPort.java

---
+++ src/main/java/com/sena/examenes/application/port/out/UsuarioRepositoryPort.java
@@ -0,0 +1,17 @@
+package com.sena.examenes.application.port.out;
+
+import com.sena.examenes.domain.model.Usuario;
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepositoryPort {
+
+    Usuario guardar(Usuario usuario);
+
+    Optional<Usuario> buscarPorUsername(String username);
+
+    List<Usuario> listarActivos();
+
+    boolean existePorUsername(String username);
+
+}

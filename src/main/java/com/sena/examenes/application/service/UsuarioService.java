fa0835ce7a17a59819451d65ff8b37b45d86ef3f
Brandonpyjv <brandonar1997@gmail.com>
2026-08-12 10:30:16 -0500

    Update UsuarioService.java

--- src/main/java/com/sena/examenes/application/service/UsuarioService.java
+++ src/main/java/com/sena/examenes/application/service/UsuarioService.java
@@ -1,38 +1,37 @@
 package com.sena.examenes.application.service;
 
 import com.sena.examenes.application.port.in.UsuarioUseCase;
+import com.sena.examenes.application.port.out.UsuarioRepositoryPort;
 import com.sena.examenes.domain.model.Usuario;
-import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UsuarioService implements UsuarioUseCase {
 
-    private final List<Usuario> usuarios = new ArrayList<>();
+    private final UsuarioRepositoryPort usuarioRepositoryPort;
 
+    public UsuarioService(UsuarioRepositoryPort usuarioRepositoryPort) {
+        this.usuarioRepositoryPort = usuarioRepositoryPort;
+
+    }
     @Override
     public Usuario registrar(String username, String email) {
-        boolean existe = usuarios.stream()
-                .anyMatch(u -> u.getUsername().equalsIgnoreCase(username));
-        if (existe) {
+        if (usuarioRepositoryPort.existePorUsername(username)) {
             throw new IllegalStateException("Ya existe un usuario con ese username.");
         }
         Usuario nuevo = new Usuario(username, email);
-        usuarios.add(nuevo);
-        return nuevo;
+        return usuarioRepositoryPort.guardar(nuevo);
     }
-
+    
     @Override
     public Optional<Usuario> buscarPorUsername(String username) {
-        return usuarios.stream()
-                .filter(u -> u.getUsername().equalsIgnoreCase(username))
-                .findFirst();
+        return usuarioRepositoryPort.buscarPorUsername(username);
     }
 
     @Override
     public List<Usuario> listarActivos() {
-        return usuarios.stream()
-                .filter(Usuario::isActivo)
-                .toList();
+        return usuarioRepositoryPort.listarActivos();
     }
 }
